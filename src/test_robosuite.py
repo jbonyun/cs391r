f9a6a6b49865ba17bb2793d9ddcc2a9c80145b85
Jeff Bonyun <jbonyun@utexas.edu>
2022-09-25 01:08:16 -0500

    Add demo of arm in space, no gravity, shooting ball

--- src/test_robosuite.py
+++ src/test_robosuite.py
@@ -1,58 +1,53 @@
 #! /usr/bin/env python3
+
 import ipdb
 
 from robosuite.models import MujocoWorldBase
 
 world = MujocoWorldBase()
 
-#from robosuite.models.base import MujocoXML
-#xml = MujocoXML('example.xml')
-#world.merge(xml)
+from robosuite.models.base import MujocoXML
+xml = MujocoXML('empty_space.xml')
+world.merge(xml)
 
-from robosuite.models.robots import Panda,IIWA
-
-mujoco_robot = IIWA()  # Iiwa will stay balanced in straight up direction
-#mujoco_robot = Panda()  # Panda will humorously flop over if you don't give it control
-
-from robosuite.models.grippers import gripper_factory
-
-gripper = gripper_factory('PandaGripper')
-mujoco_robot.add_gripper(gripper)
-
-mujoco_robot.set_base_xpos([0, 0, 0])
+# Put a robot in the world
+from robosuite.models.robots import IIWA
+mujoco_robot = IIWA()
+mujoco_robot.set_base_xpos([0, 0, 0])  # Robot is at 0,0,0 in world coords.
 world.merge(mujoco_robot)
 
-from robosuite.models.arenas import TableArena
+# Disable gravity by setting it to zero acceleration in x/y/z
+world.option.attrib['gravity'] = '0 0 0'
 
-mujoco_arena = TableArena()
-mujoco_arena.set_origin([0.8, 0, 0])  # Not sure what this is doing
-world.merge(mujoco_arena)
-
+# Insert a ball
 from robosuite.models.objects import BallObject
-from robosuite.utils.mjcf_utils import new_joint
-
 sphere = BallObject(
     name='ball',
     size=[0.02],  # ping pong is 40mm diameter = 2cm radius
     rgba=[0, 0.5, 0.5, 1],
     solref=[-10000., -7.],  # set bouncyness as negative numbers. first is stiffness, second is damping.
     ).get_obj()
-sphere.set('pos', '1.0 0 1.0')  # table is something under z=1, so z=1 will be above the table
+sphere.set('pos', '0.5 0 1.0')  # table is something under z=1, so z=1 will be above the table
 world.worldbody.append(sphere)
 
+# Create the force the propels the ball to its initial velocity
+import xml.etree.ElementTree as ET
+sphere_shooter = ET.Element('general', attrib={'name': 'shooter', 'site': 'ball_default_site'})
+world.actuator.append(sphere_shooter)
+SHOOTER_FORCE = -2.
+
+# Convert the xml that robosuite has managed into a real mujoco object
 model = world.get_model(mode="mujoco_py")
 
-# How can we set initial velocity of the ball?
-# In raw mujoco you can set the qvel property of mjData object.
-#sphere.set('qvel', '0.5 0.5 0')
+#ipdb.set_trace()
 
 from mujoco_py import MjSim, MjViewer
-
 sim = MjSim(model)
 viewer = MjViewer(sim)
 viewer.vopt.geomgroup[0] = 0 # disable visualization of collision mesh
 
 for i in range(10000):
-  sim.data.ctrl[:] = 0
+  if sim.data.ctrl is not None: sim.data.ctrl[:] = 0  # I think this makes all control efforts 0
+  if i == 0: sim.data.ctrl[model.actuator_name2id('shooter')] = SHOOTER_FORCE  # Initial force only happens on frame 0
   sim.step()
   viewer.render()
