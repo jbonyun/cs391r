3def9bc4e1620d6b44c26a75b24b6291f18b8a10
Jeff Bonyun <jbonyun@utexas.edu>
2022-11-17 22:16:09 -0600

    Remove space_arena.py
    This is not how we make our envs. We use xml and tweak from that.

--- src/space_arena.py
+++
@@ -1,9 +0,0 @@
-from robosuite.models.arenas import Arena
-
-class SpaceArena(Arena):
-    """A truly empty arena of empty space and no gravity."""
-
-    def __init__(self):
-        super().__init__('empty_space.xml')
-        # Disable gravity by setting it to zero acceleration in x/y/z
-        self.option.attrib['gravity'] = '0 0 0'
